6f51beba19ec0eef811959a469731fe266450a59
Aykut5647 <157456537+Aykut5647@users.noreply.github.com>
2024-05-13 12:43:50 +0300

    Add files via upload

---
+++ Bankaa/BankaAccount.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bankaa
+{
+    internal class BankaAccount
+    {
+        public static double VkaraniPari { get; set; }
+    }
+}
---
+++ Bankaa/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Bankaa
+{ 
+    internal class Poleta
+    {
+        public static string Ime { get; set; }
+        public static string Data { get; set; }
+    }
+
+    internal class Program 
+    {
+      
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kolko dushi shte vuvejdash?");
+            int n = int.Parse(Console.ReadLine());
+            string[] ime = new string[n];
+            string[] data=new string[n];
+            for (int i=0;i<n;i++)
+            {
+                Console.WriteLine("Vuvedi ime; ");
+                Poleta.Ime = Console.ReadLine();
+                ime[i] = Poleta.Ime;
+
+                Console.WriteLine("Vuvedi data;");
+                Poleta.Data = Console.ReadLine();
+                ime[i]= Poleta.Data;
+            }
+
+        }
+    }
+}
